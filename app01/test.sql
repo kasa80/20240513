80f2d26fe8a2eab0286c64464511ed258a84242a
kasa80 <103493850+kasa80@users.noreply.github.com>
2024-05-13 05:31:08 +0900

    Create test.sql

---
+++ app01/test.sql
@@ -0,0 +1,15 @@
+
+drop table db01;
+
+-- aaa
+create table db01
+ (
+a01		int,
+a02		varchar(10)
+ );
+
+-- insert into test_master (id,code,name,updated) values (2,'t200','namet200',current_timestamp());
+
+insert into db01 (a01,a02) values (1,'t100');
+insert into db01 (a01,a02) values (2,'t200');
+
