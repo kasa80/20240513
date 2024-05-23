3e9927351fc61b0f0b6e281d9e23355d7ba1d970
kasa80 <103493850+kasa80@users.noreply.github.com>
2024-05-24 05:20:32 +0900

    Create MockitoSubCalc.java

---
+++ Mockito/MockitoSubCalc.java
@@ -0,0 +1,18 @@
+package app01;
+
+public class MockitoSubCalc {
+
+	public int getValA(){
+    	// まだ開発途中
+        return 1;
+    }
+    public int getValB(){
+    	// まだ開発途中
+    	return 1;
+    }
+    public int getValC(int x, int y){
+    	// まだ開発途中
+    	return 1;
+    }
+
+}
